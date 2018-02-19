27a013e1cdc57971fd1fc40d4e0b0fdd67758633
bakatrouble <bakatrouble@gmail.com>
2018-02-19 14:22:43 +0300

    Fix appconfig for users

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/apps.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
-    name = '{{cookiecutter.project_slug}}.users'
+    name = 'users'
     verbose_name = "Users"
 
     def ready(self):
