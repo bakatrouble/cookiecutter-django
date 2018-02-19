7131e5aedd1516a43cce923fe8358f6ca775a516
bakatrouble <bakatrouble@gmail.com>
2018-02-19 17:27:39 +0300

    Fixes

--- {{cookiecutter.project_slug}}/manage.py
+++ {{cookiecutter.project_slug}}/manage.py
@@ -2,8 +2,13 @@
 import os
 import sys
 
+import environ
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+    env = environ.Env()
+    env.read_env()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          env("DJANGO_SETTINGS_MODULE", default="config.settings.local"))
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
